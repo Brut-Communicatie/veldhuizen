787edfab8773362f00df04ae4435facf6502e67c
TMB <tmb@TMB-1.local>
2021-04-14 15:22:05 +0200

    asd

---
+++ veldhuizen-product.php
@@ -0,0 +1,11 @@
+<?php 
+//Template name: Overzicht pagina
+//Template Post Type: post, page, product
+get_header();?>
+
+
+<div class="containerTest">
+    <?php the_content();?>
+</div>
+
+<?php get_footer();?>
