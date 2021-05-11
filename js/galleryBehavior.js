2db79588cc0c5ebc76558a4316302496403f17ed
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro.local>
2021-05-11 13:32:49 +0200

    js

--- js/galleryBehavior.js
+++ js/galleryBehavior.js
@@ -85,3 +85,8 @@ const contactButton = document.getElementById("productContactButton")
 contactButton.addEventListener('click', () => {
     document.location.href = "https://veldhuizen.nl/contact"
 })
+
+const infoContainer = document.getElementsByClassName('veldhuizen__container--product-info')[0];
+const infoContainerHTML = infoContainer.innerHTML();
+console.log(infoContainer);
+console.log(infoContainerHTML);
