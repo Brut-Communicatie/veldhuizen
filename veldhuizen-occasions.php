3b769c72c2698e4c18eacf1e9c83ae1edab479cb
Leroy Davidson Descelles <leroy@Leroys-MBP-3.fritz.box>
2021-05-24 10:50:28 +0200

    occassions

---
+++ veldhuizen-occasions.php
@@ -0,0 +1,8 @@
+<?php
+//Template name: Veldhuizen iFrame
+//Template Post Type: page
+get_header();
+
+<iframe id="voorraadFrame" src="https://voorraad.veldhuizen.nl/" width="100%" height="37902" style="height: 37902px;"></iframe>
+
+get_footer();?>
