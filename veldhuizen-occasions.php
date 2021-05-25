b963503b55f9d9fa6e7b99be3da84db15a4b1bdc
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro-3.local>
2021-05-25 09:07:30 +0200

    test

--- veldhuizen-occasions.php
+++ veldhuizen-occasions.php
@@ -1,6 +1,7 @@
 <?php
 //Template name: Veldhuizen iFrame
 //Template Post Type: page
+get_header();
 if ( have_posts() ) : while ( have_posts() ) : the_post();
 the_content();
 endwhile;
@@ -8,5 +9,10 @@ endif;
 ?>
 
 <iframe id="voorraadFrame" src="https://voorraad.veldhuizen.nl/" width="100%" height="1000px"></iframe>
+<script>
+    var frame = document.getElementById("voorraadFrame");
+var header = frame.contentWindow.document.getElementsByTagName("header")[0];
+el.style.display = "none";
+</script>
 <?php
 get_footer();?>
