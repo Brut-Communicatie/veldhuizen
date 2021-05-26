ddb5fc5f43a398f4b5cd43df4928a1749faddbe9
khualu <andrespint@gmail.com>
2021-05-26 15:14:10 +0200

    First commit, just created the template

---
+++ veldhuizen-header-info.php
@@ -0,0 +1,23 @@
+<?php
+// Template name: Header information pages
+get_header();?>
+
+    <?php 
+    
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
+        echo '<div class="top__banner"><div class="top__content"><h1>'. $post->post_title  .'</h1></div></div>';
+        echo '<div class="header-information-pages">';
+        echo '<div class=veldhuizen__container>';
+        the_content();
+        echo '</div>';
+        echo '</div>';
+        endwhile;
+        endif;
+    
+    ?>
+
+
+<?php 
+// wp_enqueue_script( 'veldhuizen-voorraad', get_template_directory_uri() . '/js/voorraad.js', false, false );
+get_footer();
+?>
