3938e52b81ecf397782bc2d88390c18f68fe88b9
khualu <andrespint@gmail.com>
2021-05-28 14:53:20 +0200

    Created a simple php template for the contact page, with an iframe for the map

---
+++ veldhuizen-contact.php
@@ -0,0 +1,28 @@
+<?php
+//Template name: Veldhuizen Contact
+//Template Post Type: page
+get_header();
+
+if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+echo '<div class="top__banner"><div class="top__content"><h1>'. $post->post_title  .'</h1></div></div>';
+echo '<iframe src="https://www.google.com/maps/d/embed?mid=1XdRdVIOV-_crTh9H9_flts_8_mQ" width="100%" height="400"></iframe>';
+echo '<div class=veldhuizen-contact>';
+echo '<div class=veldhuizen__container>';
+the_content();
+echo '</div>';
+echo '</div>';
+
+echo '<div class="service-pages">';
+echo '<div class="reparatie-formulier">';
+echo '<h2>Contactformulier</h2>';
+echo '<div class="veldhuizen__container">';
+// echo do_shortcode('[contact-form-7 id="19040" title="Reparatieformulier"]');
+echo do_shortcode('[contact-form-7 id="19180" title="Contact formulier"]');
+echo '</div>';
+echo '</div>'; 
+echo '</div>'; 
+
+endwhile;
+endif;
+?>
+
