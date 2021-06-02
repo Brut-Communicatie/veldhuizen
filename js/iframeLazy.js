e466e4386b1c0f6c22fa40897fe1f51cc3f09871
khualu <andrespint@gmail.com>
2021-06-02 17:14:19 +0200

    Working with youtube video IDs to make all videos render and work in a modal

--- js/iframeLazy.js
+++ js/iframeLazy.js
@@ -1,3 +1,35 @@
-console.log("POEP")
-let containerIframes = document.getElementsByClassName('veldhuizen__container')
-console.log(containerIframes.innerHTML)
+let modalOpen = false
+let openID
+
+const openIframe = (event) => {
+    if (modalOpen === false) {
+        document.getElementById('youtubeModal').style.display = 'block'
+        
+        let iframe = document.getElementById('youtubeIframe')
+        // console.log(iframe)
+        iframe.id = event.target.id
+        iframe.src = 'https://youtube.com/embed/' + event.target.id
+        openID = event.target.id
+        return openID
+    } else {
+        console.log('Modal closed')
+    }
+}
+
+const slide = () => {
+    let youtubeSources = document.getElementsByClassName('youtubeThumb')
+    youtubeSources = Object.values(youtubeSources) //  console.log(youtubeSources[0].src)
+
+    let index = youtubeSources.map(e => e['id']).indexOf(openID)
+    console.log(index)
+}
+
+const closeModal = () => {
+    document.getElementById("youtubeModal").style.display = "none"
+    let resetID = document.getElementsByTagName('iframe')
+    console.log(resetID)
+    resetID.id = 'youtubeIframe'
+    console.log(resetID)
+    modalOpen = false
+    // openId = null
+}
