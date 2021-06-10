c4ab05f807e56683bb682e8bf64c8ae1743faf01
khualu <andrespint@gmail.com>
2021-06-10 12:19:13 +0200

    Added a script to galleryBehavior to always get the first image to print fromt the gallery-pictures class. Also removed some console.logs in both files

--- js/productEuro.js
+++ js/productEuro.js
@@ -7,7 +7,7 @@ newStr = newStr.replace(/=/g, "=</var>")
 
 const index = newStr.search('<br><strong>Meerprijs voor:</strong>')
 let priceStr = newStr.slice(index)
-console.log(priceStr)
+// console.log(priceStr)
 newStr = newStr.replace(priceStr, "")
 
 priceStr = priceStr.replace(/–<br>/g, "<br>- ")
