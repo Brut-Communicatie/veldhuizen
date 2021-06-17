bf896299d172bd2d6eb7165f94300b19139a2d8e
khualu <andrespint@gmail.com>
2021-06-17 17:05:26 +0200

    Fun with the cutter, need to fix some stuff for it to be fully viable and scaling the excerpt both ways (expanding and shriking the width)

--- js/excerptCutter.js
+++ js/excerptCutter.js
@@ -1,11 +1,17 @@
-// console.log('poep')
 let excerpts = document.getElementsByClassName('article-excerpt')
+const originalExcerpts = document.getElementsByClassName('article-excerpt')
 const winWidth = window.innerWidth
+
+// if (!originalExcerpts) {
+//     originalExcerpts = excerpts
+// }
+
 const cutExcerpt = (charAmount) => {
     for ( x = 0; x < excerpts.length; x++ ) {
         let articleText = excerpts[x].textContent
         articleText = articleText.substr(0, charAmount)
-        excerpts[x].textContent = articleText + '...'
+        articleText += '...'
+        excerpts[x].textContent = articleText
     }
 }
 
@@ -14,5 +20,19 @@ window.onload = () => {
         cutExcerpt(150)
      } else if (winWidth <= 450) {
          cutExcerpt(100)
-     }
+    }
 }
+
+// Even fixen dat er 1 read is voor originals
+// dan kan de original weer ingezet worden als de width boven 600 komt oid
+// window.onresize = () => {
+//     const winWidth = window.innerWidth
+//     console.log(originalExcerpts[0].textContent)
+//     if (winWidth <= 600  && winWidth > 450) {
+//         cutExcerpt(150)
+//      } else if (winWidth <= 450) {
+//          cutExcerpt(100)
+//     } else {
+//         console.log(originalExcerpts[0].textContent)
+//     }
+// }
