8c2bd9716f057109b5ed63942580497c5b7bb4b0
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro-4.local>
2021-06-28 16:22:18 +0200

    cart styling

--- page.php
+++ page.php
@@ -14,15 +14,23 @@
 
 get_header();
 ?>
+<?php 
 
+
+;?>
 	<main id="primary" class="site-main">
 
 		<?php
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			if (is_cart()) {
+				// Get WooCommerce cart layout
+				get_template_part('template-parts/woocommerce/content', 'cart');
+			} 
 
+			get_template_part( 'template-parts/content', 'page' );
+			
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
@@ -34,5 +42,5 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
