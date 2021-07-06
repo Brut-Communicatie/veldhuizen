73cf7e83fa3121381cb43cb98f4d5c7ed4649cb0
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro-4.local>
2021-07-06 12:04:28 +0200

    checkout

--- template-parts/woocommerce/layout-checkout.php
+++ template-parts/woocommerce/layout-checkout.php
@@ -1,5 +1,15 @@
 <?php 
 
-echo '<div class="checkout">';
+echo '<div class="checkout__layout">';
+
+echo '<p>Bestellingen die voor 15:00 uur geplaatst zijn worden de volgende werkdag voor 17:00 uur afgeleverd, mits de onderdelen bij ons in voorraad zijn. Bij de bestellingen moet duidelijk en volledig het afleveradres (geen postbus adres) vermeld worden. Het is belangrijk dat u een adres opgeeft waar altijd iemand aanwezig is om de bestelling in ontvangst te nemen. Dit omdat er getekend moet worden voor ontvangst.</p>';
+
+echo '<p>Naast uw vestigingsadres (waar de factuur naartoe gestuurd wordt) kunt u ook een apart verzendadres invoeren.
+Het is tevens mogelijk bestellingen af te halen. Gebruik bij bestellingen altijd onze artikelnummers.
+U kunt natuurlijk ook even bellen om uw bestelling door te geven.</p>';
+
+echo '<p><strong>Ontvangt u liever eerst een prijsopgave? Klik dan de optie ‘Ik ontvang graag een prijsopgave’ in het bestelformulier aan.</strong></p>';
+echo '<div class="checkout__layout--inner">';
 the_content();
+echo '</div>';
 echo '</div>';
