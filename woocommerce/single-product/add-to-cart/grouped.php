1cddd397890315cf33505b5e8e4f06b94bfdb76d
khualu <andrespint@gmail.com>
2021-07-08 11:29:36 +0200

    fixing pull problems

--- woocommerce/single-product/add-to-cart/grouped.php
+++ woocommerce/single-product/add-to-cart/grouped.php
@@ -75,9 +75,7 @@ if ($post_title === 'trekkerombouw' or $post_title === 'verlichting' or $post_ti
 
 $assen_titles = array('d-as', 'e-as', 'f-as', 'g-as', 'remklauw-type', 'h-as', 'j60-as', 'l-as', 'r-en-v-as-1800-kg', 'r-as-1500-kg', 's-as', 't-as', 'u-as', 'w-as', 'x-as-h-naaf', 'x-as-s205-naaf');
 
-	if (in_array($post_title, $assen_titles) === true) {
-		echo '<h1>YEY</h1>';
-	}
+	
 	?>
 	<div class="veldhuizen__container one-col small">
 	<p class="bold center">Voer het aantal onderdelen in en plaats deze in het winkelmandje (oranje knop). Uw bestelling kunt u bekijken via het winkelmandje rechtsbovenaan de pagina.</p>
