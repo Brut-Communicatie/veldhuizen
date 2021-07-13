b74b204b4654879a84e3b9133161e37d77568016
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro-4.local>
2021-07-13 14:56:22 +0200

    Styling of your order overview for checkout

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -17,14 +17,12 @@
 
 defined( 'ABSPATH' ) || exit;
 ?>
-<table class="shop_table woocommerce-checkout-review-order-table">
-	<thead>
-		<tr>
-			<th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
-			<th class="product-total"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
-		</tr>
-	</thead>
-	<tbody>
+
+		<div class="checkout__product--top">
+			<div class="checkout__product--heading"><?php esc_html_e( 'Product', 'woocommerce' ); ?></div>
+			<div class="checkout__product--heading"><?php esc_html_e( 'Artikelnummer', 'woocommerce' ); ?></div>
+			<div class="checkout__product--price"><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></div>
+		</div>
 		<?php
 		do_action( 'woocommerce_review_order_before_cart_contents' );
 
@@ -32,30 +30,36 @@ defined( 'ABSPATH' ) || exit;
 			$_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
 
 			if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
-				?>
-				<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
-					<td class="product-name">
+				$_artikelnummer = get_post_meta( $_product->get_id(), '_artikelnummer', true );
+			?>
+	
+				<div class="checkout__product">
+					<div class="checkout__product--item">
 						<?php echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) ) . '&nbsp;'; ?>
 						<?php echo apply_filters( 'woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf( '&times;&nbsp;%s', $cart_item['quantity'] ) . '</strong>', $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-						<?php echo wc_get_formatted_cart_item_data( $cart_item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-					</td>
-					<td class="product-total">
+					</div>
+
+					<div class="checkout__product--item">
+					<?php echo $_artikelnummer ;?>
+					</div>
+
+					<div class="checkout__product--price">
 						<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-					</td>
-				</tr>
+					</div>
+				</div>
 				<?php
 			}
 		}
 
 		do_action( 'woocommerce_review_order_after_cart_contents' );
 		?>
-	</tbody>
-	<tfoot>
 
-		<tr class="cart-subtotal">
+	<div class="checkout__product--bottom">
+
+		<div class="checkout__product--subtotal">
 			<th><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
 			<td><?php wc_cart_totals_subtotal_html(); ?></td>
-		</tr>
+		</div>
 
 		<?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
 			<tr class="cart-discount coupon-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
@@ -99,12 +103,11 @@ defined( 'ABSPATH' ) || exit;
 
 		<?php do_action( 'woocommerce_review_order_before_order_total' ); ?>
 
-		<tr class="order-total">
+		<div class="checkout__product--total">
 			<th><?php esc_html_e( 'Total', 'woocommerce' ); ?></th>
 			<td><?php wc_cart_totals_order_total_html(); ?></td>
-		</tr>
+		</div>
 
 		<?php do_action( 'woocommerce_review_order_after_order_total' ); ?>
-
-	</tfoot>
-</table>
+	</div>
+<!-- </table> -->
